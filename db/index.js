b8a7a9e9948764be5409b7800a1530468e1ebd7f
Anton Savcheko <savchenko.inc@gmail.com>
2015-11-16 20:26:26 +0200

    change  API

--- db/index.js
+++ db/index.js
@@ -1,34 +1,38 @@
 var mysql = require('mysql');
 
+//var connection  = mysql.createConnection({
+//        host        :   'sql4.freemysqlhosting.net',
+//        user        :   'sql496545',
+//        password    :   'c2HXNikVMn',
+//        database    :   'sql496545'
+//    });
 var connection  = mysql.createConnection({
-        host        :   'sql4.freemysqlhosting.net',
-        user        :   'sql496545',
-        password    :   'c2HXNikVMn',
-        database    :   'sql496545'
-    });
-
+    host        :   'localhost',
+    user        :   'root',
+    password    :   'BanderaMozli1994',
+    database    :   'company'
+});
 
 module.exports = {
     users: function (res) {
         var query = connection.query('select * from user', function (err, rows, fields) {
             if (err) throw err;
-            res.send(JSON.stringify(rows));
+            res.send(rows);
         });
     },
     user:function (req, res) {
-        console.log(req.param("email"));
-        if(req.param("id")){
-            var query = connection.query('select * from user where user_id ='+req.param("id"), function (err, rows, fields) {
-                if (err) throw err;
-                res.send(JSON.stringify(rows));
-            });
+        try{
+            if(req.param("password") && req.param("email")){
+                var query = connection.query('select * from user where password =\''+req.param("password")+'\' and email='+req.param("email"), function (err, rows, fields) {
+                    if (err) throw err;
+                    if(rows[0]) res.send('Yes');
+                    else res.send('No');
+                });
+            }
+        }catch(err){
+            res.send("It`s wrong request!!! Be careful")
         }
-        if(req.param("email")){
-            var query = connection.query('select * from user where email ='+req.param("email"), function (err, rows, fields) {
-                if (err) throw err;
-                res.send(JSON.stringify(rows));
-            });
-        }
+
     },
     adverts: function (res) {
         var query = connection.query('select * from advert', function (err, rows, fields) {
