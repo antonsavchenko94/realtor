d06eb3183fad949e58e1b0903c88d1034bac0a9b
antonsavchenko94 <savchenko.inc@gmail.com>
2015-11-30 16:28:13 +0200

    change db config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,19 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host'        :   'localhost',
-        'user'        :   'root',
-        'password'    :   'BanderaMozli1994',
+        'host'        :   'db4free.net',
+        'user'       :   'mozli',
+        'password'    :   'qwertyui'
     },
-	'database': 'company',
+	'database': 'realtor_company',
     'users_table': 'user'
 };
+//module.exports = {
+//    'connection': {
+//            'host'        :   'localhost',
+//            'user'        :   'root',
+//            'password'    :   'BanderaMozli1994',
+//},
+//    'database': 'company',
+//    'users_table': 'user'
+//};
